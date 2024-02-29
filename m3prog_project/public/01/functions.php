89a11ce9557d2e153c40ac1dbbfe8110b159cb19
Robin Lemmers <38210@ma-web.nl>
2024-02-29 15:26:03 +0100

    add br

--- m3prog_project/public/01/functions.php
+++ m3prog_project/public/01/functions.php
@@ -2,11 +2,13 @@
     // strtoupper
     $lowercaseString = "string";
     echo strtoupper($lowercaseString);
+    echo "<br>";
 
     // strpos
     $string = "dit is een lange string met meerdere woorden er in en zo";
     $stringToFind = "woorden";
     echo strpos($string, $stringToFind);
+    echo "<br>";
 
     // file_exists
     $filePath = "functions.md";
