5c5706a003f72dd875a3f9e1311b3d2df887dafe
Robin Lemmers <38210@ma-web.nl>
2024-03-07 10:25:34 +0100

    02 variabelen

---
+++ m3prog_project/public/02/variabelen.php
@@ -0,0 +1,16 @@
+<?php
+    echo $auto2 = "auto2";
+    echo "<br>";
+    echo $_naam = "_naam";
+    echo "<br>";
+    //echo $auto's = "auto's";
+    //echo $auto/onderdelen = "onderdelen";
+    //echo $huis&tuin = "huis en tuin";
+    echo $naw = "naw";
+    echo "<br>";
+    //echo $22 = 22;
+    //echo $$auto = "auto";
+    echo $kleurVanAuto = "rood";
+    echo "<br>";
+    //echo $software-dev = "software-dev";
+?>
