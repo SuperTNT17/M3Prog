a526fb1ae94890309aba6647d49fe77dd16e7a9c
Robin Lemmers <38210@ma-web.nl>
2024-03-28 14:38:03 +0100

    02function-args

--- m3prog_project/public/04/functions.php
+++ m3prog_project/public/04/functions.php
@@ -22,4 +22,11 @@ function multiply($getal1, $getal2){
     $som = $getal1 * $getal2;
     return $som;
 }
+
+function berekenRitKosten($afstand_km, $km_per_liter, $liter_prijs){
+    $kosten_euro = 0;
+    $aantal_liters = $afstand_km / $km_per_liter;
+    $kosten_euro = $aantal_liters * $liter_prijs;
+    return $kosten_euro;
+}
 ?>
--- m3prog_project/public/04/script.php
+++ m3prog_project/public/04/script.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../assets/css/04script.css">
+</head>
+<body>
 <?php
 include_once 'functions.php';
 echo currentDateTime() . "</br>";
@@ -5,3 +14,11 @@ echo add(1, 2) . "</br>";
 echo subtract(1, 2) . "</br>";
 echo divide(1, 2) . "</br>";
 echo multiply(1, 2) . "</br>";
+
+echo "<h2>ritkosten</h2>";
+echo "<p>€" . number_format(berekenRitKosten(663, 15, 1.90), 2, ",", ".") . "</p>";
+echo "<p>€" . number_format(berekenRitKosten(1438, 20, 1.46), 2, ",", ".") . "</p>";
+echo "<p>€" . number_format(berekenRitKosten(1991, 18.47, 2.23), 2, ",", ".") . "</p>";
+?>
+</body>
+</html>
